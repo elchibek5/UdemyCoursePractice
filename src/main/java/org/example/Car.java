872d530b8497006f12fe1b32e0c5b80348b2adb1
elchibekdastanov <elchibekdastanov@gmail.com>
2025-05-21 23:57:25 -0500

    Initial commit

---
+++ src/main/java/org/example/Car.java
@@ -0,0 +1,38 @@
+package org.example;
+
+public class Car {
+    private String make = "BMW";
+    private String model = "X7";
+    private String color = "Pink";
+    private int doors = 4;
+    private boolean convertible = true;
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public boolean isConvertible() {
+        return convertible;
+    }
+
+    public void describeCar() {
+        System.out.println(doors + "-Door " +
+                color + " " +
+                make + " " +
+                model + " " +
+                (convertible ? "Convertible" : ""));
+    }
+
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Car car = new Car();
+//        car.describeCar();
+
+        System.out.println("make " + car.getMake());
+        System.out.println("color " + car.getColor());
+        System.out.println("model " + car.getModel());
+        System.out.println("door " + car.getDoors());
+
+        car.describeCar();
+
+
+    }
+}
