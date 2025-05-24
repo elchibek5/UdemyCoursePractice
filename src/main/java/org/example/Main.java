7f075a4f1ef0c5ba403468f748fbc236b904bed9
elchibekdastanov <elchibekdastanov@gmail.com>
2025-05-23 22:16:17 -0500

    OOP practice

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,13 +6,13 @@ public class Main {
         Car car = new Car();
 //        car.describeCar();
 
+
         System.out.println("make " + car.getMake());
         System.out.println("color " + car.getColor());
         System.out.println("model " + car.getModel());
         System.out.println("door " + car.getDoors());
 
         car.describeCar();
-
 
 
     }
