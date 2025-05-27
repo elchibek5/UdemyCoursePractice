8fb7281c224b55eeff56bd7dd6d411615052387c
elchibekdastanov <elchibekdastanov@gmail.com>
2025-05-26 22:16:25 -0500

    Implementing basic calculator

--- src/main/java/problems/BasicCalculator.java
+++ src/main/java/problems/BasicCalculator.java
@@ -26,6 +26,19 @@ public class BasicCalculator {
             return firstNumber + secondNumber;
         }
 
+        public double getSubtractionResult() {
+            return firstNumber - secondNumber;
+        }
 
+        public double getMultiplicationResult() {
+            return firstNumber * secondNumber;
+        }
+
+        public double getDivisionResult() {
+            if (secondNumber == 0) {
+                return 0;
+            }
+            return firstNumber / secondNumber;
+        }
     }
 }
