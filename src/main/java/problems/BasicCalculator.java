5f80ed99e7cd5bf0d9d5167812a9772f58db4d5d
elchibekdastanov <elchibekdastanov@gmail.com>
2025-05-26 22:15:44 -0500

    Implementing basic calculator

---
+++ src/main/java/problems/BasicCalculator.java
@@ -0,0 +1,31 @@
+package problems;
+
+public class BasicCalculator {
+    public class SimpleCalculator {
+        // write code here
+        private double firstNumber;
+        private double secondNumber;
+
+        public double getFirstNumber() {
+            return firstNumber;
+        }
+
+        public double getSecondNumber() {
+            return secondNumber;
+        }
+
+        public void setFirstNumber(double number) {
+            this.firstNumber = number;
+        }
+
+        public void setSecondNumber(double number) {
+            this.secondNumber = number;
+        }
+
+        public double getAdditionResult() {
+            return firstNumber + secondNumber;
+        }
+
+
+    }
+}
