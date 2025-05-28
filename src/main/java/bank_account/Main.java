fb8b05e46efd8e0f3099b4065db9bb38a647c4ae
elchibekdastanov <elchibekdastanov@gmail.com>
2025-05-28 00:56:37 -0500

    Practicing class fields and instance methods

--- src/main/java/bank_account/Main.java
+++ src/main/java/bank_account/Main.java
@@ -2,13 +2,17 @@ package bank_account;
 
 public class Main {
     public static void main(String[] args) {
-        Account ericsAccount = new Account();
 
-        ericsAccount.setBalance(200);
-        ericsAccount.setCustomerEmail("eric2@gmail.com");
-        ericsAccount.setCustomerName("Eric");
-        ericsAccount.setNumber("12123");
-        ericsAccount.setCustomerPhone("+10293943090");
+        Account ericsAccount = new Account("12345123", 1000.000, "eric Eric",
+                "eric@gmail.com", "+08871232");
+
+        System.out.println(ericsAccount.getNumber());
+        System.out.println(ericsAccount.getBalance());
+//        ericsAccount.setBalance(200);
+//        ericsAccount.setCustomerEmail("eric2@gmail.com");
+//        ericsAccount.setCustomerName("Eric");
+//        ericsAccount.setNumber("12123");
+//        ericsAccount.setCustomerPhone("+10293943090");
 
 
 
