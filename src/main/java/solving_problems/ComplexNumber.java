d806ac2875e2eb0c85e30c63f4d27920e8e543ef
elchibekdastanov <elchibekdastanov@gmail.com>
2025-06-03 17:53:35 -0500

    Build a class to handle addition, subtraction, and representation of complex numbers, reinforcing method overloading and object manipulation.

---
+++ src/main/java/solving_problems/ComplexNumber.java
@@ -0,0 +1,38 @@
+package solving_problems;
+
+public class ComplexNumber {
+
+        private double real;
+        private double imaginary;
+
+        public ComplexNumber(double real, double imaginary) {
+            this.real = real;
+            this.imaginary = imaginary;
+        }
+
+        public double getReal() {
+            return real;
+        }
+
+        public double getImaginary() {
+            return imaginary;
+        }
+
+        public void add(double real, double imaginary) {
+            this.real += real;
+            this.imaginary += imaginary;
+        }
+
+        public void add(ComplexNumber num) {
+            add(num.real, num.imaginary);
+        }
+
+        public void subtract(double real, double imaginary) {
+            this.real -= real;
+            this.imaginary -= imaginary;
+        }
+
+        public void subtract(ComplexNumber other) {
+            subtract(other.real, other.imaginary);
+    }
+}
