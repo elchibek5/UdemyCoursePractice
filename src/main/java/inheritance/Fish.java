0bed5ae1285fa49ef95c88a113699c997cd5cf3f
elchibekdastanov <elchibekdastanov@gmail.com>
2025-06-27 21:36:13 -0500

    Dog Class

--- src/main/java/inheritance/Fish.java
+++ src/main/java/inheritance/Fish.java
@@ -5,9 +5,35 @@ public class Fish extends Animal {
     private int gills;
     private int fins;
 
-    public Fish(String type, String size, double weight, int gills, int fins) {
+    public Fish(String type, double weight, int gills, int fins) {
         super(type, "small", weight);
         this.gills = gills;
         this.fins = fins;
+    }
+
+    private void moveMuscles() {
+        System.out.print("muscles moving ");
+    }
+
+    private void moveBackFin() {
+        System.out.print("backfin moving ");
+    }
+
+    @Override
+    public String toString() {
+        return "Fish{" +
+                "gills=" + gills +
+                ", fins=" + fins +
+                "} " + super.toString();
+    }
+
+    @Override
+    public void move(String speed) {
+        super.move(speed);
+        moveMuscles();
+        if (speed == "fast") {
+            moveBackFin();
+        }
+        System.out.println();
     }
 }
