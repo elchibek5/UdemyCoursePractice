8bf7075bc41fafc6862759e2556046506a407a1d
elchibekdastanov <elchibekdastanov@gmail.com>
2025-07-04 16:50:13 -0500

    Practicing OOP

--- src/main/java/InheritanceChallenge/Main.java
+++ src/main/java/InheritanceChallenge/Main.java
@@ -1,4 +1,15 @@
 package InheritanceChallenge;
 
 public class Main {
+    public static void main(String[] args) {
+        Employee tim = new Employee("11/11/2000", "tim",
+                "07/04/2025");
+        System.out.println(tim);
+        System.out.println("Age = " + tim.getAge());
+        System.out.println("Pay = " + tim.collectPay());
+
+        Employee joe = new Employee("Joe", "11/11/2000",
+                "09/09/2025");
+        System.out.println(joe);
+    }
 }
