f4a9a615adff79f3d5b112b01178eee439a05152
elchibekdastanov <elchibekdastanov@gmail.com>
2025-07-04 17:17:42 -0500

    Practicing as giving paycheck to employees

--- src/main/java/InheritanceChallenge/Main.java
+++ src/main/java/InheritanceChallenge/Main.java
@@ -8,8 +8,12 @@ public class Main {
         System.out.println("Age = " + tim.getAge());
         System.out.println("Pay = " + tim.collectPay());
 
-        Employee joe = new Employee("Joe", "11/11/2000",
-                "09/09/2025");
+        SalariedEmployee joe = new SalariedEmployee("Joe", "11/11/2000",
+                "09/09/2025", 35000);
         System.out.println(joe);
+        System.out.println("Joe's Paycheck = $" + joe.collectPay());
+
+        joe.retire();
+        System.out.println("Joe's Pension check = $" + joe.collectPay());
     }
 }
