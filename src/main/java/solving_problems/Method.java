bb10d6e0bb99ae650fb9070c626276c3eb3db8bc
elchibekdastanov <elchibekdastanov@gmail.com>
2025-07-06 23:33:05 -0500

    simple comparison

---
+++ src/main/java/solving_problems/Method.java
@@ -0,0 +1,11 @@
+package solving_problems;
+
+public class Method {
+    public static int max(int num1, int num2) {
+        if (num1 > num2) {
+            return num1;
+        } else {
+            return num2;
+        }
+    }
+}
