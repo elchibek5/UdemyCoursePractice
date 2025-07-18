19617da94f88a0acf61e4704bc97c25e25e97733
elchibekdastanov <elchibekdastanov@gmail.com>
2025-07-18 17:21:01 -0500

    Practicing getters and setters

---
+++ src/main/java/OOPBook/OOP.java
@@ -0,0 +1,21 @@
+package OOPBook;
+
+public class OOP {
+    public class Entry {
+        private String name;
+        private String number;
+
+        public void setName(String person) {
+            name = person;
+        }
+        public String getName() {
+            return(name);
+        }
+        public void setNumber(String phone) {
+            number = phone;
+        }
+        public String getNumber() {
+            return(number);
+        }
+    }
+}
